3505510ebf1b6939a3e1c88e54208604745c73e6
Dan Johnson <dan.johnson@automattic.com>
2016-09-19 10:21:12 -0700

    Adds suggestions to the post edit screen

--- proofreading.php
+++ proofreading.php
@@ -8,7 +8,7 @@ Author:      Dan Johnson
 Author URI:  
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: 
+Text Domain: proofreading
 Domain Path: /languages
 */
 
@@ -18,20 +18,23 @@ class Proofreading {
 
 		add_filter( 'the_content', array( $this, 'output_suggestion_form' ) );
 		add_action( 'init', array( $this , 'add_new_suggestion' ) );
+		add_action( 'add_meta_boxes', array( $this , 'add_suggestion_meta_box' ) );
 		error_log(print_r($_POST, true));
 
 	}
 
 	function output_suggestion_form( $content ) {
 		$postid = get_the_ID();
+		$form_title = '<h3>' . esc_attr__( 'Spotted a typo? Let us know about it.', 'proofreading') . '</h3>';
 		$form = '<form method="POST" action="" class="typo-suggestion-form">
-					<input type="text" name="suggester_name" placeholder="Your name" />
-					<input type="text" name="suggester_email" placeholder="Your email" />
-					<textarea name="suggestion" rows=4 maxlength=100 placeholder="Please enter a correction for this post"></textarea>
+					<input type="text" name="suggester_name" placeholder="' . esc_attr__( 'Your name', 'proofreading' ) . '" />
+					<input type="text" name="suggester_email" placeholder="' . esc_attr__( 'Your email', 'proofreading' ) . '" />
+					<textarea name="suggestion" rows=4 maxlength=100 placeholder="' . esc_attr__( 'Please enter a correction for this post', 'proofreading' ) . '"></textarea>
 					<input type="hidden" name="postid" value="' . $postid . '" />' 
-					. wp_nonce_field( 'add_suggestion', '_suggestion_nonce' ) 
-					. '<input type="submit" text="Suggest" />
+					. wp_nonce_field( 'add_suggestion_to' . $postid, '_suggestion_nonce' ) 
+					. '<input type="submit" text="' . esc_attr__( 'Suggest', 'proofreading' ) . '" />
 				</form>';
+		$content .= $form_title;
 		$content .= $form;
 		return $content;
 	}
@@ -39,7 +42,7 @@ class Proofreading {
 	function add_new_suggestion() {
 
 		if( isset($_POST) && ! empty($_POST) ) {
-			if ( wp_verify_nonce( $_POST["_suggestion_nonce"], 'add_suggestion' ) ) {
+			if ( wp_verify_nonce( $_POST["_suggestion_nonce"], 'add_suggestion_to' . $_POST["postid"] ) ) {
 
 				$userid = get_current_user_id();
 				$postid = $_POST["postid"];
@@ -55,14 +58,48 @@ class Proofreading {
 					'comment_content' => $suggestion,
 					'comment_type' => 'suggestion',
 					'comment_parent' => 0,
-					'user_id' => $userid
+					'user_id' => $userid,
+					'comment_approved' => 'suggested',
+					'comment_status' => 'suggested'
 				);
 
 				//Insert new comment and get the comment ID
-				wp_new_comment( $commentdata );
+				wp_insert_comment( $commentdata );
 			}
+		}
+	}
+
+	function add_suggestion_meta_box() {
+		global $post;
+		$postid = $post->ID;
+		add_meta_box(
+	        'suggestions',
+	        __( 'Suggestions', 'proofreading' ),
+	        array( $this, 'display_suggestions_in_post_edit_screen'),
+	        'post',
+	        'normal',
+	        'low',
+	        array( 'id' => $postid )
+	    );
+	}
+
+	function display_suggestions_in_post_edit_screen( $post, $metabox ) {
+		$args = array(
+			'status' => 'suggested',
+			'post_id' => $metabox['args']['id'],
+			'type' => 'suggestion'
+		);
+		$suggestions = get_comments($args);
+		if( is_array($suggestions) && empty($suggestions) ) {
+			esc_attr_e('No suggestions yet.', 'proofreading');
+		} else {
+			echo '<ul class="suggestion-list">';
+			foreach($suggestions as $suggestion) :
+				echo( '<li>Name: ' . $suggestion->comment_author . '<br />Suggestion: ' . $suggestion->comment_content . '</li>');
+			endforeach;
+			echo '</ul>';
 		}
 	}
 }
 
-$proofreading = new Proofreading();
+new Proofreading();
